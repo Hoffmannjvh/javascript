b8710daa10eb3c68734272d0a38cfeb90dbbbc7b
João Vitor Hoffmann <contato.jhonnybjj@gmail.com>
2022-01-24 15:07:08 -0300

    att

--- alura/ex001/07-condicionais.js
+++ alura/ex001/07-condicionais.js
@@ -5,26 +5,43 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 17;
-const estaAcompanhada = true
-
+const idadeComprador = 15;
+const estaAcompanhada = false
+const temPassagemComprada = true
 
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade")
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade")
+//     listaDeDestinos.splice(1, 1);
+// } else if (estaAcompanhada) {
+//     console.log("Comprador está acompanhado")
+//     listaDeDestinos.splice(1, 1);
+// } else {
+//     console.log("Comprador não é maior de idade e não posso vender")
+
+// }
+    
+
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem")
     listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhada) {
-    console.log("Comprador está acompanhado")
-    listaDeDestinos.splice(1, 1);
-} else {
+}
+else {
     console.log("Comprador não é maior de idade e não posso vender")
 
 }
-    
 
+
+console.log("Embarque: \n")
+if(idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa viagem!")
+}else{
+    console.log("Você não pode embarcar")
+}
 
 console.log(listaDeDestinos);
 
