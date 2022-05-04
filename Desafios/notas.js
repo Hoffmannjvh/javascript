3d124d8c2d932621a27a560e02842328d3040d57
João Vitor Hoffmann <contato.jhonnybjj@gmail.com>
2022-05-04 13:37:11 -0300

    att

---
+++ Desafios/notas.js
@@ -0,0 +1,17 @@
+
+
+// const notas = [10, 6, 8, 7];
+
+// let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+//console.log(media)
+
+const notas = [10, 6, 8];
+
+notas.push(7);
+
+console.log(notas)
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
